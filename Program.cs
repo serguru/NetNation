c259dbb8e37b8234cbfab8292839bb6b1d0bf879
Sergey Chernyshov <serguru@gmail.com>
2025-04-16 12:23:05 -0700

    The code is ready for testing

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ using System.Text;
 public class Program
 {
     #region helpers
-    private static string ValidateAndSplit(string row, int rowNUmber, out string[] arr)
+
+    /// <summary>
+    /// Validatates string and splits it to an array
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="rowNUmber"></param>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static string ValidateAndSplit(string row, int rowNUmber, out string[] arr)
     {
         // Ensure 'arr' is assigned before returning
         arr = Array.Empty<string>();
@@ -46,8 +54,19 @@ public class Program
 
     }
 
-    private static int TryParseInt(string s)
+    /// <summary>
+    /// Converts nullable string to int?
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidDataException"></exception>
+    public static int? TryParseInt(string? s)
     {
+        if (String.IsNullOrEmpty(s))
+        {
+            return null;
+        }
+
         bool valid = int.TryParse(s, out int result);
         if (valid)
         {
@@ -57,21 +76,40 @@ public class Program
         throw new InvalidDataException($"Wrong integer encountered: '{s}'");
     }
 
-    private static Guid TryParseGuid(string s)
+    /// <summary>
+    /// Converts nullable string to Guid?
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidDataException"></exception>
+    public static Guid? TryParseGuid(string? s)
     {
+        if (String.IsNullOrEmpty(s))
+        {
+            return null;
+        }
         bool valid = Guid.TryParse(s, out Guid result);
         if (valid)
         {
             return result;
         }
         ;
-        throw new InvalidDataException($"Wrong integer encountered: '{s}'");
+        throw new InvalidDataException($"Wrong Guid encountered: '{s}'");
     }
 
-    private static string Guid2Str(Guid guid)
+    /// <summary>
+    /// Converts nullable Guid to string?
+    /// </summary>
+    /// <param name="guid"></param>
+    /// <returns></returns>
+    public static string? Guid2Str(Guid? guid)
     {
+        if (!guid.HasValue)
+        {
+            return null;
+        }
         // Leave alphanumeric only
-        string s = Regex.Replace(guid.ToString(), "[^a-zA-Z0-9]", "");
+        string s = Regex.Replace(guid.Value.ToString(), "[^a-zA-Z0-9]", "");
         if (s.Length == 32)
         {
             return s;
@@ -83,26 +121,37 @@ public class Program
         return s.PadRight(32, '0');
     }
 
-    static Dictionary<string, int> countToUsage = new Dictionary<string, int>()
-        {
-            { "EA000001GB0O", 1000 },
-            { "PMQ00005GB0R", 5000 },
-            { "SSX006NR", 1000 },
-            { "SPQ00001MB0R", 2000 }
-        };
-
-
-    // itemCount converter
-    public static int ConvertItemCount(string partNumber, int itemCount)
+    public static Dictionary<string, int> countToUsage = new()
     {
+        { "EA000001GB0O", 1000 },
+        { "PMQ00005GB0R", 5000 },
+        { "SSX006NR", 1000 },
+        { "SPQ00001MB0R", 2000 }
+    };
 
-        if (!countToUsage.ContainsKey(partNumber))
+
+    /// <summary>
+    /// Converts itemCount to usage
+    /// </summary>
+    /// <param name="partNumber"></param>
+    /// <param name="itemCount"></param>
+    /// <returns></returns>
+    public static int? ConvertItemCount(string? partNumber, int? itemCount)
+    {
+        if (String.IsNullOrEmpty(partNumber))
+        {
+            return null;
+        }
+        if (!itemCount.HasValue)
+        {
+            return null;
+        }
+        if (!countToUsage.ContainsKey(partNumber.ToUpper()))
         {
             return itemCount;
         }
 
         return itemCount / countToUsage[partNumber];
-
     }
 
     #endregion
@@ -155,18 +204,35 @@ public class Program
 
         int[] excludePartnerIds = config.GetSection("ExcludePartnerIds").Get<int[]>() ?? [];
 
-        StringBuilder sbChargeable = new StringBuilder("insert into chargeable (partnerID, product, partnerPurchasedPlanID, [plan], usage) values\n");
+        StringBuilder sbChargeable = new StringBuilder("INSERT INTO chargeable (partnerID, product, partnerPurchasedPlanID, [plan], usage) VALUES\n");
 
         // A collection to build the 'domains' insert statement
-        Dictionary<string, string> domains = new();
+        Dictionary<string, string?> domains = new();
 
         // constants
         const string comma = ",";
         const string singleQuote = "'";
-
+        const string nullString = "NULL";
 
         // Read the data file, parse field values, append sbChargeable
-        int index = -1;
+
+        /*
+            0: PartnerID    -> partnerID,        
+            1: partnerGuid,
+            2: accountid,
+            3: accountGuid  -> partnerPurchasedPlanID,
+            4: username,
+            5: domains,
+            6: itemname,
+            7: plan         -> plan,
+            8: itemType,
+            9: PartNumber   -> product
+           10: itemCount    -> usage
+        */
+
+        var runningCounts = new List<(int RowNo, string Product, int ItemCount, int RunningTotal)>();
+
+        int index = 0;
         foreach (var row in File.ReadLines(dataPath))
         {
             index++;
@@ -177,63 +243,124 @@ public class Program
                 throw new InvalidDataException(message);
             }
 
-            if (index < 1)
+            if (index == 1)
             {
                 continue;
             }
 
-            int partnerId = TryParseInt(arr[0]);
-            if (excludePartnerIds.Contains(partnerId))
+            // Excluding rows for patherIds included into
+            // "ExcludePartnerIds" section in appsettings.json
+            int? partnerId = TryParseInt(arr[0]);
+            if (partnerId.HasValue && excludePartnerIds.Contains(partnerId.Value))
             {
                 continue;
             }
-
-            int itemCount = TryParseInt(arr[10]);
 
             if (String.IsNullOrEmpty(arr[9]))
             {
                 logMessages.Add($"Row #{index} was skipped because of missing PartNumber");
+                continue;
             }
-            else if (!mappingDictionary.ContainsKey(arr[9]))
+
+            if (!mappingDictionary.ContainsKey(arr[9]))
             {
                 logMessages.Add($"Row #{index} was skipped because of missing PartNumber '{arr[9]}'");
+                continue;
             }
-            else if (itemCount <= 0)
+
+            int? itemCount = TryParseInt(arr[10]);
+
+            if (itemCount <= 0)
             {
                 logMessages.Add($"Row #{index} was skipped because of non-positive itemCount");
+                continue;
             }
-            else
+
+            string partnerIdStr = partnerId.HasValue ? partnerId.Value.ToString() : nullString;
+
+            string? partnerPurchasedPlanID = Guid2Str(TryParseGuid(arr[3]));
+
+            int? usage = ConvertItemCount(arr[10], itemCount);
+
+            string usageStr = usage.HasValue ? usage.Value.ToString() : nullString;
+
+            string? product = mappingDictionary[arr[9]];
+
+            try
             {
-                string partnerPurchasedPlanID = Guid2Str(TryParseGuid(arr[3]));
-                string domain = arr[5];
-                try
+                // open values
+                sbChargeable.Append("(");
+
+                // partnerID
+                sbChargeable.Append(partnerIdStr);
+                sbChargeable.Append(comma);
+
+                // product
+                if (String.IsNullOrWhiteSpace(product))
                 {
-                    sbChargeable.Append("(");
-                    sbChargeable.Append(partnerId);
-                    sbChargeable.Append(comma);
+                    sbChargeable.Append(nullString);
+                }
+                else
+                {
+                    product = product.Trim();
                     sbChargeable.Append(singleQuote);
-                    sbChargeable.Append(mappingDictionary[arr[9]]);
+                    sbChargeable.Append(product);
                     sbChargeable.Append(singleQuote);
-                    sbChargeable.Append(comma);
+                }
+                sbChargeable.Append(comma);
+
+                // partnerPurchasedPlanID
+                if (String.IsNullOrWhiteSpace(partnerPurchasedPlanID))
+                {
+                    sbChargeable.Append(nullString);
+                }
+                else
+                {
                     sbChargeable.Append(singleQuote);
-                    sbChargeable.Append(partnerPurchasedPlanID);
+                    sbChargeable.Append(partnerPurchasedPlanID.Trim());
                     sbChargeable.Append(singleQuote);
-                    sbChargeable.Append(comma);
-                    sbChargeable.Append(TryParseInt(arr[8]));
-                    sbChargeable.Append(comma);
-                    sbChargeable.Append(ConvertItemCount(arr[9], itemCount));
-                    sbChargeable.Append("),\n");
+                }
+                sbChargeable.Append(comma);
+
+                // plan
+                string? plan = arr[7];
+                if (String.IsNullOrWhiteSpace(plan))
+                {
+                    sbChargeable.Append(nullString);
+                }
+                else
+                {
+                    sbChargeable.Append(singleQuote);
+                    sbChargeable.Append(plan.Trim());
+                    sbChargeable.Append(singleQuote);
+                }
+                sbChargeable.Append(comma);
+
+                // usage
+                sbChargeable.Append(usageStr);
+
+                // close values
+                sbChargeable.Append("),\n");
+            }
+            catch (Exception e)
+            {
+                throw new InvalidDataException($"Error data parsing in a row {index} with message {e.Message}");
+            }
+
+            if (!String.IsNullOrWhiteSpace(product) && itemCount.HasValue)
+            {
+                runningCounts.Add((index, product, itemCount.Value, 0));
+            }
 
 
-                    if (!domains.ContainsKey(domain))
-                    {
-                        domains.Add(domain, partnerPurchasedPlanID);
-                    }
-                }
-                catch (Exception e)
-                {
-                    throw new InvalidDataException($"Error data parsing in a row {index} with message {e.Message}");
-                }
+            string domain = arr[5];
+            if (String.IsNullOrWhiteSpace(domain))
+            {
+                throw new InvalidDataException("Domain cannot be empty");
+            }
+            if (!domains.ContainsKey(domain))
+            {
+                domains.Add(domain, partnerPurchasedPlanID);
             }
         }
 
@@ -241,10 +368,7 @@ public class Program
         sbChargeable.Append(";");
         string insertChargeable = sbChargeable.ToString();
 
-
-
-
-        StringBuilder sbDomains = new StringBuilder("insert into domains (domain, partnerPurchasedPlanID) values\n");
+        StringBuilder sbDomains = new StringBuilder("INSERT INTO domains (domain, partnerPurchasedPlanID) VALUES\n");
 
         foreach (var item in domains)
         {
@@ -252,25 +376,57 @@ public class Program
             sbDomains.Append(item.Key);
             sbDomains.Append(singleQuote);
             sbDomains.Append(comma);
-            sbDomains.Append(singleQuote);
-            sbDomains.Append(item.Value);
-            sbDomains.Append("'),\n");
+            if (String.IsNullOrWhiteSpace(item.Value))
+            {
+                sbDomains.Append(nullString);
+            }
+            else
+            {
+                sbDomains.Append(singleQuote);
+                sbDomains.Append(item.Value);
+                sbDomains.Append(singleQuote);
+            }
+            sbDomains.Append("),\n");
         }
         sbDomains.Remove(sbDomains.Length - 2, 2);
         sbDomains.Append(";");
         string insertDomains = sbDomains.ToString();
 
+        List<string> runningCountsStr = new()
+        {
+        "Product, ItemCount, RunningTotal, RowNo"
+        };
+
+        runningCountsStr.AddRange(runningCounts
+            .OrderBy(x => x.Product)
+            .ThenBy(x => x.RowNo)
+            .GroupBy(s => s.Product)
+            .SelectMany(g =>
+            {
+                int cumulative = 0;
+                return g.OrderBy(s => s.RowNo)
+                        .Select(s =>
+                        {
+                            cumulative += s.ItemCount;
+                            return (s.RowNo, s.Product, s.ItemCount, cumulative);
+                        });
+            })
+            .Select(x => $"{x.Product}, {x.ItemCount}, {x.cumulative}, {x.RowNo}")
+            .ToList());
 
         const string s1 = "insert-chargeable.sql";
         const string s2 = "insert-domains.sql";
         const string s3 = "log-errors.txt";
+        const string s4 = "log-success.txt";
 
         File.WriteAllText(s1, insertChargeable);
         File.WriteAllText(s2, insertDomains);
         File.WriteAllLines(s3, logMessages);
+        File.WriteAllLines(s4, runningCountsStr);
 
         Console.WriteLine($"Insert statements generated. Please see files {s1} and {s2}.");
         Console.WriteLine($"Please find error messages in {s3}");
+        Console.WriteLine($"Please find success messages in {s4}");
     }
 }
 
